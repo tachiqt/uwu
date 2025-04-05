c19a65d6d34bd434e5a0ba0faf557ba6f747d9c9
tachiqt <markvincentperez2@gmail.com>
2025-04-05 14:26:27 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Arduino_Csharp_Serial_Communication_Control_a_Servo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_open = new System.Windows.Forms.Button();
+            this.comboBox_portLists = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.EventDataGridView = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EventDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_open);
+            this.groupBox2.Controls.Add(this.comboBox_portLists);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.groupBox2.Location = new System.Drawing.Point(42, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 112);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "COM PORT";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // button_open
+            // 
+            this.button_open.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.button_open.FlatAppearance.BorderSize = 0;
+            this.button_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_open.Location = new System.Drawing.Point(196, 44);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(120, 40);
+            this.button_open.TabIndex = 1;
+            this.button_open.Text = "Connect";
+            this.button_open.UseVisualStyleBackColor = false;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // comboBox_portLists
+            // 
+            this.comboBox_portLists.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_portLists.FormattingEnabled = true;
+            this.comboBox_portLists.Location = new System.Drawing.Point(41, 44);
+            this.comboBox_portLists.Name = "comboBox_portLists";
+            this.comboBox_portLists.Size = new System.Drawing.Size(121, 36);
+            this.comboBox_portLists.TabIndex = 0;
+            this.comboBox_portLists.DropDown += new System.EventHandler(this.comboBox_portLists_DropDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // EventDataGridView
+            // 
+            this.EventDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EventDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.EventDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.EventDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EventDataGridView.Location = new System.Drawing.Point(32, 214);
+            this.EventDataGridView.Name = "EventDataGridView";
+            this.EventDataGridView.RowHeadersWidth = 70;
+            this.EventDataGridView.RowTemplate.Height = 28;
+            this.EventDataGridView.Size = new System.Drawing.Size(1357, 259);
+            this.EventDataGridView.TabIndex = 6;
+            this.EventDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.EventDataGridView_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 34);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(36, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 28);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Scan RFID : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(575, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 56);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 56);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "90";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(1370, 608);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EventDataGridView);
+            this.Controls.Add(this.groupBox2);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.EventDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.ComboBox comboBox_portLists;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView EventDataGridView;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using Arduino_Csharp_Serial_Communication_Control_a_Servo.Repository;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Arduino_Csharp_Serial_Communication_Control_a_Servo
+{
+    public partial class Form1 : Form
+    {
+        private bool isProcessing = false;
+        private string lastScannedID = string.Empty;
+        private DateTime lastScanTime = DateTime.MinValue;
+        private int scanCooldownMs = 3000; // 3 seconds cooldown between scans
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textBox1.Focus();
+        }
+
+        private void comboBox_portLists_DropDown(object sender, EventArgs e)
+        {
+            string[] portLists = SerialPort.GetPortNames();
+            comboBox_portLists.Items.Clear();
+            comboBox_portLists.Items.AddRange(portLists);
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox_portLists.Text;
+                serialPort1.BaudRate = 57600;
+                serialPort1.Open();
+
+                string str_degree = "0";
+
+                serialPort1.Write(str_degree + "\n");
+                MessageBox.Show("Success Connected to Arduino Board");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && !isProcessing)
+            {
+                string currentID = textBox1.Text;
+                DateTime currentTime = DateTime.Now;
+
+                
+                if (currentID == lastScannedID &&
+                    (currentTime - lastScanTime).TotalMilliseconds < scanCooldownMs)
+                {
+                    
+                    textBox1.Text = "";
+                    return;
+                }
+
+                isProcessing = true;
+
+                try
+                {
+                    lastScannedID = currentID;
+                    lastScanTime = currentTime;
+
+                    using (SqlConnection con = new SqlConnection(AppHelper.ConnectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("SELECT * FROM Details WHERE ID =@ID"))
+                        {
+                            cmd.Parameters.AddWithValue("@ID", textBox1.Text);
+                            cmd.CommandType = CommandType.Text;
+                            cmd.Connection = con;
+                            con.Open();
+                            using (SqlDataReader sdr = cmd.ExecuteReader())
+                            {
+                                if (sdr.Read() && sdr.HasRows)
+                                {
+                                    string statID = sdr["ID"].ToString();
+                                    string statName = sdr["Name"].ToString();
+                                    string stat = sdr["Status"].ToString().Trim();
+                                    if (stat.Equals("Logged", StringComparison.OrdinalIgnoreCase))
+                                    {
+
+                                        using (SqlConnection sqlConnection = new SqlConnection(AppHelper.ConnectionString))
+                                        {
+                                            using (SqlCommand sqlCmdEdit = new SqlCommand("Update dbo.Details set Status='Out' where ID=@ID", sqlConnection))
+                                            {
+                                                sqlCmdEdit.Parameters.AddWithValue("@ID", textBox1.Text);
+                                                sqlConnection.Open();
+                                                sqlCmdEdit.ExecuteNonQuery();
+                                                sqlConnection.Close();
+
+
+                                                RefreshDataGridView(textBox1.Text);
+
+                                                if (serialPort1.IsOpen)
+                                                {
+                                                    serialPort1.Write(180 + "\n");
+                                                }
+                                            }
+                                        }
+
+
+                                        using (SqlConnection sqlConnection = new SqlConnection(AppHelper.ConnectionString))
+                                        {
+                                            sqlConnection.Open();
+                                            DateTime currentDate = DateTime.Now;
+                                            using (SqlCommand sqlCmd = new SqlCommand("Insert into tbllogsheet(ID,Name,TimeOut)values(@ID,@Name,@CurrentDate)", sqlConnection))
+                                            {
+                                                sqlCmd.Parameters.AddWithValue("@ID", textBox1.Text);
+                                                sqlCmd.Parameters.AddWithValue("@Name", statName);
+                                                sqlCmd.Parameters.AddWithValue("@CurrentDate", currentDate);
+                                                sqlCmd.ExecuteNonQuery();
+                                            }
+                                            sqlConnection.Close();
+                                        }
+                                    }
+                                    else
+                                    {
+                                
+                                        bool canTimeIn = true;
+                                        DateTime today = DateTime.Now.Date;
+
+                                        using (SqlConnection sqlConnection = new SqlConnection(AppHelper.ConnectionString))
+                                        {
+                                            string checkQuery = @"
+                                                SELECT TOP 1 TimeOut 
+                                                FROM tbllogsheet 
+                                                WHERE ID=@ID 
+                                                  AND CONVERT(date, TimeOut) = @CurrentDate 
+                                                ORDER BY TimeOut DESC";
+
+                                            using (SqlCommand checkCmd = new SqlCommand(checkQuery, sqlConnection))
+                                            {
+                                                checkCmd.Parameters.AddWithValue("@ID", textBox1.Text);
+                                                checkCmd.Parameters.AddWithValue("@CurrentDate", today);
+
+                                                sqlConnection.Open();
+                                                object result = checkCmd.ExecuteScalar();
+                                                sqlConnection.Close();
+
+                                                if (result != null && result != DBNull.Value)
+                                                {
+                                                    
+                                                    canTimeIn = false;
+                                                }
+                                            }
+                                        }
+
+                                        if (!canTimeIn)
+                                        {
+                                            MessageBox.Show("You have already timed out today. You can only time in again tomorrow.");
+                                            textBox1.Text = "";
+                                            isProcessing = false;
+                                            return;
+                                        }
+
+                                        using (SqlConnection sqlConnection = new SqlConnection(AppHelper.ConnectionString))
+                                        {
+                                            using (SqlCommand sqlCmdEdit = new SqlCommand("Update dbo.Details set Status='Logged' where ID=@ID", sqlConnection))
+                                            {
+                                                sqlCmdEdit.Parameters.AddWithValue("@ID", textBox1.Text);
+                                                sqlConnection.Open();
+                                                sqlCmdEdit.ExecuteNonQuery();
+                                                sqlConnection.Close();
+
+
+                                                RefreshDataGridView(textBox1.Text);
+
+                                                if (serialPort1.IsOpen)
+                                                {
+                                                    serialPort1.Write(0 + "\n");
+                                                }
+                                            }
+                                        }
+
+
+                                        using (SqlConnection sqlConnection = new SqlConnection(AppHelper.ConnectionString))
+                                        {
+                                            sqlConnection.Open();
+                                            DateTime currentDate = DateTime.Now;
+                                            using (SqlCommand sqlCmd = new SqlCommand("Insert into tbllogsheet(ID,Name,TimeIn)values(@ID,@Name,@CurrentDate)", sqlConnection))
+                                            {
+                                                sqlCmd.Parameters.AddWithValue("@ID", textBox1.Text);
+                                                sqlCmd.Parameters.AddWithValue("@Name", statName);
+                                                sqlCmd.Parameters.AddWithValue("@CurrentDate", currentDate);
+                                                sqlCmd.ExecuteNonQuery();
+                                            }
+                                            sqlConnection.Close();
+                                        }
+                                    }
+
+                                    textBox1.Text = "";
+                                }
+                                else
+                                {
+                                    textBox1.Text = "";
+                                    MessageBox.Show("ID not Found Please Register!");
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing scan: " + ex.Message);
+                }
+                finally
+                {
+                    isProcessing = false;
+                }
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(0 + "\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(90 + "\n");
+        }
+
+        private void EventDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void RefreshDataGridView(string id)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Details where ID=@ID", AppHelper.ConnectionString);
+            da.SelectCommand.Parameters.AddWithValue("@ID", id);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Details");
+
+            EventDataGridView.DataSource = ds.Tables["Details"].DefaultView;
+            EventDataGridView.Update();
+            EventDataGridView.Refresh();
+        }
+    }
+}
